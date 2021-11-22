8d26cd6cce815d8f7936e23e57b56803cb3d0563
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-11-23 03:28:24 +0900

    Add reqeustList, checkedRequest, confirmRequest, selectChild

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('user/request/', views.CreateRequest.as_view()),
     path('user/result/', views.showRequestList.as_view()),
     path('user/result/<int:postID>/', views.showRequest.as_view()),
-    path('master/checkRequest/', views.checkRequest.as_view()),
-    path('master/checkRequest/<int:postID>/', views.confirmRequest.as_view()),
+    path('master/checkRequest/', views.requestList.as_view()),
+    path('master/checkRequest/<int:postID>/', views.checkedRequest.as_view()),
+    path('master/confirmRequest/<int:postID>/', views.confirmRequest.as_view()),
+    path('master/selectChild/', views.selectChild.as_view()),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -15,29 +15,30 @@ class CheckLogin(generics.CreateAPIView):
         queryset = User.objects.filter(email=email)  # user_id에 위에서 얻어온 id값이 들어가야 함
         if queryset:
             if queryset.values()[0].get('password') == password:
-                return JsonResponse({'message' : '로그인 성공', 'data' : queryset.values()[0]}, status = 200)
+                return JsonResponse({'message' : 'success', 'data' : queryset.values()[0]}, status = 200)
         # id 존재하지 않음
-        return JsonResponse({'message' : '아이디 혹은 비밀번호가 올바르지 않습니다'}, status= 200)
-        
+        return JsonResponse({'message' : 'incorrect'}, status= 200)
+
 class CreateNewUser(generics.CreateAPIView):
     def post(self, request):
         data = json.loads(request.body)
         user = User()
         user.email = data['email']
         if User.objects.filter(email = data['email']).exists():
-            return JsonResponse({'message' : '이미 가입된 이메일입니다.'}, status = 200)
+            return JsonResponse({'message' : 'duplication'}, status = 200)
         user.password = data['password']
         user.name = data['name']
         user.phone = data['phone']
         user.child_name = data['child_name']
         user.save()
-        return JsonResponse({'message': '회원가입 성공'}, status = 200)
+        return JsonResponse({'message': 'success'}, status = 200)
 
 class CreateRequest(generics.CreateAPIView):
     def post(self, request):
         data = json.loads(request.body)
         req = Request()
         req.user_id = data['session_id']
+        req.user_name = data['user_name']
         req.request_date = data['request_date']
         req.center_name = data['center_name']
         req.date = data['date']
@@ -46,10 +47,10 @@ class CreateRequest(generics.CreateAPIView):
         req.process_state = data['process_state']
         req.check = data['check']
         req.save()
-        return JsonResponse({'message' : '게시글이 정상적으로 작성되었습니다.'}, status = 200)
+        return JsonResponse({'message' : 'success'}, status = 200)
 
 #사용자의 전체 요청 기록 리스트
-class showRequestList(generics.ListCreateAPIView): 
+class showRequestList(generics.ListCreateAPIView):
     def post(self, request):
         data = json.loads(request.body)
         queryset = Request.objects.filter(user_id=data['session_id'])
@@ -60,16 +61,45 @@ class showRequest(generics.CreateAPIView):
     def post(self, request, postID = False):
         data = json.loads(request.body)
         queryset = Request.objects.filter(user_id=data['session_id'])
+        # 복호화키 찾아서 front 넘겨주기. output_video 테이블에서 request_id 로 찾아서 넘겨주면 될듯
         post_num = self.kwargs['postID']
         return JsonResponse(queryset.values()[post_num - 1], status = 200)
 
 
 #전체 사용자의 요청 기록 리스트
-class checkRequest(generics.ListCreateAPIView):
+class requestList(generics.ListCreateAPIView):
     def get(self, request):
         queryset = Request.objects.all()
+        # print(queryset.values())
         return JsonResponse(list(queryset.values()), status = 200, safe = False)
 
 #사용자 요청 기록 승인 및 거부
+class checkedRequest(generics.CreateAPIView):
+    def post(self, request, postID = False):
+        queryset = Request.objects.all()
+        post_num = self.kwargs['postID']
+        return JsonResponse(queryset.values()[post_num - 1], status = 200)
+
 class confirmRequest(generics.CreateAPIView):
-    QuerySet = ''
+    def post(self, request, postID = False):
+        data = json.loads(request.body)
+        post_num = self.kwargs['postID']
+        print(post_num)
+        req = Request.objects.get(request_id=post_num)
+        req.check = data['check']
+        req.save()
+        return JsonResponse({}, status = 200)
+
+class selectChild(generics.CreateAPIView):
+    def post(self, request):
+        data = json.loads(request.body)
+        output = OutputVideo()
+        output.request_id = data['request_id']
+        #output. 비디오 아이디
+        # output.output_source : 저장할 경로..??
+        #output.decoding_key = 암호화 함수 실행 시켜서 넘어오는 data['selectedNum'] 매칭 시켜서 걔 저장
+        return JsonResponse({'message' : 'success'}, status = 200)
+
+# 암호화 함수
+
+# detect & tracking 함수
